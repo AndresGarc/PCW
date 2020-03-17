4010f0e01f7cd72e893f6b0655d04326e0abf92e
Andres <andres.garciamiras@gmail.com>
2020-03-17 15:14:06 +0100

    Index cargado

--- codigoA.js
+++ codigoA.js
@@ -21,10 +21,10 @@ function loadArt(){
       let af = document.createElement('a');
       let img = document.createElement('img');
       let foot = document.createElement('footer');
-      let nFot = document.createElement('p'); let iF = document.createElement('i');
-      let prc = document.createElement('p'); let iP = document.createElement('i');
-      let vis = document.createElement('p') ; let iV = document.createElement('i');
-      let seg  = document.createElement('p'); let iS = document.createElement('i');
+      let nFot = document.createElement('p'); let iF = document.createElement('i'); let bF = document.createElement('b');
+      let prc = document.createElement('p'); let iP = document.createElement('i'); let bP = document.createElement('b');
+      let vis = document.createElement('p') ; let iV = document.createElement('i'); let bV = document.createElement('b');
+      let seg  = document.createElement('p'); let iS = document.createElement('i'); let bS = document.createElement('b');
 
       //enlace titulo
       at.innerHTML = `${e.nombre}`;
@@ -36,10 +36,10 @@ function loadArt(){
       img.alt="imagen articulo";
 
       //cosas footer
-      iF.className = "far fa-images";
-      iP.className = "far fa-coins";
-      iV.className = "far fa-eye";
-      iS.classNmae= "far fa-users";
+      iF.className = "far fa-images"; bF.innerHTML = "Nº Fotos:";
+      iP.className = "fas fa-coins"; bP.innerHTML = "Precio:";
+      iV.className = "far fa-eye"; bV.innerHTML= "Visitas:";
+      iS.className= "fas fa-users"; bS.innerHTML= "Seguidores:";
 
       //creacion
       af.appendChild(img);
@@ -48,12 +48,20 @@ function loadArt(){
       art.className="boxProd";
       desc.className = "desc";
       desc.innerHTML = `${e.descripcion}`;
-
+      nFot.appendChild(iF); nFot.appendChild(bF); nFot.innerHTML+= ` ${e.nfotos}`;
+      foot.appendChild(nFot);
+      prc.appendChild(iP); prc.appendChild(bP); prc.innerHTML+= ` ${e.precio}`;
+      foot.appendChild(prc);
+      vis.appendChild(iV); vis.appendChild(bV); vis.innerHTML+= ` ${e.veces_visto}`;
+      foot.appendChild(vis);
+      seg.appendChild(iS); seg.appendChild(bS); seg.innerHTML+= ` ${e.nsiguiendo}`;
+      foot.appendChild(seg);
 
       //creacion del articulo
       art.appendChild(titulo);
       art.appendChild(af);
       art.appendChild(desc);
+      art.appendChild(foot);
 
       document.getElementById('flex').appendChild(art);
 
