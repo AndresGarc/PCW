7a52ccf3ff4f5d6dedcb55ba2997a8dea6afb789
Yera <ynms1@alu.ua.es>
2020-03-18 16:35:26 +0100

    Login + cerrarsesion()

--- codigoY.js
+++ codigoY.js
@@ -1,16 +1,21 @@
-function login(form) {
-    let xhr = new XMLHttpRequest(),
-    url = 'api/usuarios/login',
+function loginn(form) {
+    let url = 'api/usuarios/login',
     fd = new FormData(form);
-    xhr.open('POST',url, true);
-    
-    xhr.onload = function(){ //coger llamada servidor y comparar
-        console.log(xhr.responseText);
-        sessionStorage.setItem("name",varnomb);
-        sessionStorage.setItem("password",varpas);
-    };
-    xhr.send(fd);
-    //si el login esta en la bbdd
+
+    fetch(url, {method:'POST',
+             body:fd}).then(function(respuesta){
+        if( respuesta.ok){
+            respuesta.json().then(function(datos){
+                console.log(datos);
+                console.log(JSON.stringify(datos));
+                sessionStorage['usuario'] = JSON.stringify(datos);
+            });
+        }
+        else {
+            console.log('ERROR');
+        }
+    });
+    return false;
 }
 
 function mostrarsesion() {
@@ -22,20 +27,20 @@ function mostrarsesion() {
 }
 
 function enviarindex() { // pa esto usar un onload o algo asi en el login
-    if ((sessionStorage.name != undefined) && (sessionStorage.password != undefined)) {
-        location.href ="http://www.pagina1.com";
+    if (sessionStorage.usuario != undefined) {
+        location.href ="index.html";
     }
     else {
         window.alert("Esta linea no hace nada porque te debe dejar entrar a login si no hs abierto sesion");
     }
 }
 
-function cerrarsesion() {
-    if ((sessionStorage.name != undefined) && (sessionStorage.password != undefined)) {
+function cerrarSesion() {
+    if ((sessionStorage.usuario !=undefined)) {
         sessionStorage.clear();
-        window.alert("detecta datos, borra. Has cerrado sesion");
+        window.alert("Borramos sesion. Has cerrado sesion");
     }
-    else {  
-    window.alert("No has abierto sesion");
+    else {
+        window.alert("No has abierto sesion");
     }
 }
