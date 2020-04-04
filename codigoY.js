6d86444680bcaf78f44031fd0d881e9902b3eb0f
Yera <ynms1@alu.ua.es>
2020-04-04 21:42:55 +0200

    Articulo cargado

--- codigoY.js
+++ codigoY.js
@@ -46,9 +46,11 @@ function cerrarSesion() {
 
 function borraNavbar(borraesto) { //le pasamos por string la pag actual, tras eso comprobamos si esta logeado o no para borrar cosis
     //if si no le pasamos nada
-    var local = document.getElementById(borraesto);
-        local.parentNode.removeChild(local); //esto es una marranada pero era como estaba en muchos sitios???
 
+    if (borraesto !='') {
+        var local = document.getElementById(borraesto);
+        local.parentNode.removeChild(local); //esto es una marranada pero era como estaba en muchos sitios???
+    }
     if((sessionStorage.usuario !=undefined)) { //si esta logueado, quitamos login y registro
         var element = document.getElementById('login');
         element.parentNode.removeChild(element);
@@ -72,31 +74,111 @@ function borraNavbar(borraesto) { //le pasamos por string la pag actual, tras es
 
 // Relacionado con Articulo 
 
-function creaArticulo(art) { //probar con this
+function creaArticulo(e) {
+    
+    let art = document.createElement('article');
+    let titulo = document.createElement('h2');
+    let divv = document.createElement('div');
+    let carousel = document.createElement('aside');
+    let img = document.createElement('img');
+    let pp = document.createElement('p');
+    let btnatras = document.createElement('button'); let iconatras = document.createElement('i');
+    let span = document.createElement('span');
+    let btndelante = document.createElement('button'); let icondelante = document.createElement('i');
+    let foot = document.createElement('footer');
+    let desc = document.createElement('p'); 
+    let vend = document.createElement('p'); let iVe = document.createElement('i'); let bVe = document.createElement('b'); let url = document.createElement('a');
+    let prc = document.createElement('p'); let iP = document.createElement('i'); let bP = document.createElement('b');
+    let nFot = document.createElement('p'); let iF = document.createElement('i'); let bF = document.createElement('b');
+    let vis = document.createElement('p') ; let iV = document.createElement('i'); let bV = document.createElement('b');
+    let seg  = document.createElement('p'); let iS = document.createElement('i'); let bS = document.createElement('b');
+    let preg  = document.createElement('p'); let pregred = document.createElement('a'); let icpreg = document.createElement('i'); let bPre = document.createElement('b');
+    let botones  = document.createElement('p'); let follow = document.createElement('button'); let unf = document.createElement('button');
+  
+    //titulo
+    art.id="prod";
+    titulo.innerHTML = `${e.nombre}`;
+    divv.id="flex-prod";
+  
+    //foto
+    carousel.className="carrusel";
+    img.className = "foto";
+    img.src=`fotos/articulos/${e.imagen}`;
+    img.alt="imagen articulo";
+    btnatras.className = "moveFoto";btnatras.name="prevFoto";
+        btndelante.className = "moveFoto";btnatras.name="nextFoto"; //tipo button?
+    iconatras.className="fas fa-arrow-alt-circle-left";icondelante.className="fas fa-arrow-alt-circle-right";
+  
+    //cosas footer
+   vend.className="attbprod"; iVe.className = "fas fa-user"; bVe.innerHTML = "Vendedor/a:"; url.href="buscar.html";
+   prc.className="attbprod"; iF.className = "far fa-images"; bF.innerHTML = "Nº Fotos:";
+   nFot.className="attbprod"; iP.className = "fas fa-coins"; bP.innerHTML = "Precio:";
+   vis.className="attbprod"; iV.className = "far fa-eye"; bV.innerHTML= "Visitas:";
+   seg.className="attbprod"; iS.className= "fas fa-users"; bS.innerHTML= "Seguidores:";
+   preg.className="attbprod";   pregred.href= "#preguntas"; pregred.title="Enlace a las preguntas"; //no dirige bien???
+        icpreg.className="fas fa-question-circle"; bPre.innerHTML="Preguntas realizadas:";
+   botones.className="attbprod"; follow.innerHTML = "seguir"; unf.innerHTML = "dejar de seguir";
+  
+    //creacion
+    desc.className = "descLong";
+    desc.innerHTML = `${e.descripcion}`;
+    
+    //carrusel (dentro de div)
+    btnatras.appendChild(iconatras); btndelante.appendChild(icondelante);
+    span.innerHTML+='1 de' + ` ${e.nfotos}`; //como saber que foto es
+    pp.appendChild(btnatras);pp.appendChild(span); pp.appendChild(btndelante);
+    carousel.appendChild(img);carousel.appendChild(pp);
 
+    //footer (dentro de div)
+    foot.appendChild(desc);
+    vend.appendChild(iVe); vend.appendChild(bVe); url.innerHTML+=` ${e.vendedor}`; vend.appendChild(url);
+    foot.appendChild(vend);
+    prc.appendChild(iP); prc.appendChild(bP); prc.innerHTML+= ` ${e.precio}`;
+    foot.appendChild(prc);
+    nFot.appendChild(iF); nFot.appendChild(bF); nFot.innerHTML+= ` ${e.nfotos}`;
+    foot.appendChild(nFot);
+    vis.appendChild(iV); vis.appendChild(bV); vis.innerHTML+= ` ${e.veces_visto}`;
+    foot.appendChild(vis);
+    seg.appendChild(iS); seg.appendChild(bS); seg.innerHTML+= ` ${e.nsiguiendo}`;
+    foot.appendChild(seg);  
+    preg.appendChild(pregred);preg.appendChild(icpreg);preg.appendChild(bPre); preg.innerHTML+=` ${e.npreguntas}`;
+    foot.appendChild(preg);
+    botones.appendChild(follow);botones.appendChild(unf);
+    foot.appendChild(botones);
+  
+    //insercion en divv
+    divv.appendChild(carousel);
+    divv.appendChild(foot);
+
+    //creacion del articulo
+    art.appendChild(titulo);
+    art.appendChild(divv);
+
+    document.getElementById('flex').appendChild(art);
 }
 
+function infoArticulo(){  
+    const queryString = window.location.search; //aqui podemos hacer un metodo para sacar parametros de la URL
+    const urlParams = new URLSearchParams(queryString); //aqui podemos hacer un metodo para sacar parametros de la URL
+    const id = urlParams.get('id'); //aqui podemos hacer un metodo para sacar parametros de la URL
 
-
-function infoArticulo(){
     var comprobar = false;
-
-if(sessionStorage.usuario != undefined) {
-    let usu =   JSON.parse(sessionStorage['usuario']);
-        autorizacion = usu.login + ":" + usu.token  ;
-}
+    let autorizacion = 'usuario:token';
+    if(sessionStorage.usuario != undefined) {
+         let usu =   JSON.parse(sessionStorage['usuario']);
+            autorizacion = usu.login + ":" + usu.token  ;
+    }
 
     let xhr = new XMLHttpRequest(),
-    url= 'api/articulos/1',
-    autorizacion = 'usuario:token'; //tomando la info del sessionstorage
+    url= 'api/articulos/'+id; //tomando la info del sessionstorage
 
     xhr.open('GET',url,true);
 
     xhr.onload = function(){
         console.log(xhr.responseText);
         let r = JSON.parse(xhr.responseText);
-        console.log(r);
-        creaArticulo(r);
+        console.log(r.FILAS[0] );
+        creaArticulo(r.FILAS[0]);
     }
 
     xhr.setRequestHeader('Authorization',autorizacion);
