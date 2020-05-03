69015c8ff9a07e284f7d6b6511966df7487e5631
Yera <ynms1@alu.ua.es>
2020-05-03 16:37:23 +0200

    Sudoku cuadrado

--- Practica3/codigoY.js
+++ Practica3/codigoY.js
@@ -4,7 +4,8 @@ function cargasudoku(){ //el boton de empezar
     //desactiva elemento cambio tamaño
     //hacer peticion cargarsudoku
     let url = 'api/sudoku/generar/4';
-    fetch(url, {method:'POST'}).then(function(respuesta){
+    console.log(url);
+    fetch(url,{method:'POST'}).then(function(respuesta){
         if( respuesta.ok){
             
             respuesta.json().then(function(datos){
@@ -28,10 +29,7 @@ function cambiasudoku(){
     //esta funcion cambiaria el tamaño del grid del sudoku cada vez que se cambiase eso
 }
 
-function creacanvas(){
-    var cv = document.getElementById("canvas");
-    var context = cv.getContext('2d');
-}
+
 
 function compruebaerrores(){
     //let sudoku  sessionstorage sudoku, con eso hacemos una peticion POST a /id sudoku/comprobar
@@ -42,4 +40,49 @@ function compruebaerrores(){
 function botonfinalizar(){
     //peticion tipo delete igual que compruebaerrores
     // si respuesta.ok parar temporizador y reiniciar la pagina
+}
+
+function canvaselector(regiones){
+    let cv = document.querySelector('canvas');
+    //if regiones, entonces cambiar width y height???
+    cv.width=640;
+    cv.height=640;
+    cv.onclick = function(evento){
+        
+       
+        let alto=cv.height/regiones,
+        ancho=cv.width/regiones;
+        let columna = Math.floor(evento.offsetX/ancho),
+        fila = Math.floor(evento.offsetY/alto);
+        console.log(fila+' ' +columna);
+        
+    }
+    let subdivi = Math.sqrt(regiones);
+    crearrejilla(regiones,1);
+    crearrejilla(subdivi,3);
+    //cv.onmousemove
+    //cv.onmousedown al clickar
+    //cv.onmouseup al soltar el click
+    //cv.onmousenter  al entrar al canvas (cuando los errores), onmouseleave 
+}
+
+function crearrejilla(regiones,tam){
+    
+    let cv = document.querySelector('canvas'),
+    ctx = cv.getContext('2d'),
+    ancho=cv.width/regiones,
+    alto=cv.height/regiones;
+    ctx.beginPath();
+    ctx.lineWidth= tam;
+    ctx.strokeStyle = '#04724D';
+
+    for (let i=0;i<regiones;i++){
+        
+            ctx.moveTo(i*ancho, 0);
+            ctx.lineTo(i*ancho,cv.height);
+            ctx.moveTo(0,i*alto);
+            ctx.lineTo(cv.width,i*alto);
+        
+    }
+    ctx.stroke();
 }
